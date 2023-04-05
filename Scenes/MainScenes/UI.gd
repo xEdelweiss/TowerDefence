db09c0576da01b1544eefe21eb1d9bfefc179d68
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-04-05 21:56:08 +0300

    Додав налаштування турелей та індикатор дальності стрільби

--- Scenes/MainScenes/UI.gd
+++ Scenes/MainScenes/UI.gd
@@ -2,28 +2,39 @@ extends CanvasLayer
 
 class_name GameSceneUI
 
-const TOWER_PREVIEW = "TowerPreview"
+const TOWER_PREVIEW_CONTAINER = "TowerPreview"
 const DRAG_TOWER = "DragTower"
+const RANGE_SPRITE = "RangeSprite"
 
 func set_tower_preview(tower_type: String, mouse_position: Vector2):
 	var drag_tower = (load("res://Scenes/Turrets/" + tower_type.to_snake_case() + ".tscn") as PackedScene).instantiate() as Node2D
 	drag_tower.name = DRAG_TOWER
-	drag_tower.modulate = Color("ab54ff3c")
+
+	var range_texture = Sprite2D.new()
+	var scaling = GameData.tower_data[tower_type].range / 600.0
+	range_texture.scale = Vector2(scaling, scaling)
+	range_texture.texture = load("res://Assets/UI/range_overlay.png")
+	range_texture.name = RANGE_SPRITE
 	
-	var control = Control.new()
-	control.add_child(drag_tower, true)
-	control.position = mouse_position
-	control.name = TOWER_PREVIEW
-	add_child(control, true)
-	move_child(get_node(TOWER_PREVIEW), 0)
+	var container = Control.new()
+	
+	container.add_child(drag_tower, true)
+	container.add_child(range_texture, true)
+
+	container.position = mouse_position
+	container.name = TOWER_PREVIEW_CONTAINER
+	add_child(container, true)
+	move_child(get_node(TOWER_PREVIEW_CONTAINER), 0)
 
 func update_tower_preview(new_position: Vector2, color: String):
-	var control = get_node(TOWER_PREVIEW) as Control
+	var control = get_node(TOWER_PREVIEW_CONTAINER) as Control
 	control.position = new_position
-#
+
 	var drag_tower = control.get_node(DRAG_TOWER) as Node2D
+	var range_texture = control.get_node(RANGE_SPRITE) as Sprite2D
 	if (drag_tower.modulate != Color(color)):
 		drag_tower.modulate = Color(color)
+		range_texture.modulate = Color(color)
 
 func remove_tower_preview():
-	get_node(TOWER_PREVIEW).free()
+	get_node(TOWER_PREVIEW_CONTAINER).free()
---
+++ Singletons/game_data.gd
@@ -0,0 +1,20 @@
+extends Node
+
+# rate_of_fire - how many times per second the tower can fire
+var tower_data = {
+	"GunT1": {
+		"damage": 20,
+		"rate_of_fire": 1,
+		"range": 350,
+	},
+	"GunT2": {
+		"damage": 40,
+		"rate_of_fire": 1,
+		"range": 450,
+	},
+	"MissileT1": {
+		"damage": 100,
+		"rate_of_fire": 3,
+		"range": 550,
+	},
+}
