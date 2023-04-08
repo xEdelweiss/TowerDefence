47cdc503487216aa1e14e812577de7ef19f6df7d
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2023-04-08 17:41:54 +0300

    Додав ще дві дороги на Карті 1

--- Scenes/MainScenes/game_scene.gd
+++ Scenes/MainScenes/game_scene.gd
@@ -92,7 +92,7 @@ func start_next_wave():
 	spawn_enemies(wave_data)
 
 func retrieve_wave_data():
-	var wave_data = [["BlueTank", 1.5], ["BlueTank", 0.1]]
+	var wave_data = [["BlueTank", 1.5], ["BlueTank", 2.1], ["BlueTank", 1.5], ["BlueTank", 2.1], ["BlueTank", 1.5], ["BlueTank", 2.1], ["BlueTank", 1.5], ["BlueTank", 2.1]]
 	current_wave += 1
 	enemies_in_wave = wave_data.size()
 	return wave_data
@@ -106,4 +106,4 @@ func spawn_enemies(wave_data):
 		#yield(get_tree().create_timer(i[1]), "timeout")
 
 func get_random_enemy_path() -> Path2D:
-	return map_node.get_node(str("EnemyPath", randi_range(1, 2)))
+	return map_node.get_node(str("EnemyPath", randi_range(1, 4)))
